0768808d89a186c82c9232914745cf314fd5fbb2
JornabeDV <103864663+JornabeDV@users.noreply.github.com>
2022-07-20 18:03:08 -0300

    entregable complementario

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+// Pedir un número 5 veces mediante prompt y sumarle otro número en cada repetición, mostrar el resultado por consola, usando FOR.
+
+// let numero1 = parseInt (prompt("Ingrese el número que será sumado en cada iteración"));
+// let suma = 0;
+
+// for (let i = 0; i < 5; i++) {
+//     let numero2 = parseInt(prompt("Ingrese el número para sumar"));
+//     suma = numero1 + numero2,
+//     console.log(suma);
+
+// }
+
+//  Pedir un texto por consola, concatenar un valor en cada iteración, realizando una salida por resultado, hasta que se ingresa "CERRAR".
+
+// let texto = prompt ("Ingrese un texto");
+// let resultado = texto;
+// let textoConcatenar = " ";
+
+// while (textoConcatenar != "CERRAR") {
+//     textoConcatenar = prompt ("Ingrese un texto para concatenarle al inicial");
+//     resultado = resultado + " " + textoConcatenar;
+//     console.log(resultado);
+// }
+
+
+// Pedir un número y mostrar alerta con mensaje "CoderHouse" la cantidad de veces del valor número.
+
+// let numero1 = parseInt (prompt("Ingrese un número para repetir el mensaje"));
+
+// for (let i = 1; i <= numero1; i++) {
+// alert("CoderHouse");
+// }
+
+
+// Algoritmo que solicita una entrada al usuario hasta que ingresa "ESC".
+
+// let entrada1 = prompt("Ingresar un dato");
+// //Repetimos con While hasta que el usuario ingresa "ESC"
+// while (entrada1 != "ESC") {
+//     alert("El usuario ingresó "+ entrada1);
+//     //Volvemos a solicitar un dato. En la próxima iteración se evalúa si no es ESC.
+//     entrada1 = prompt("Ingresar otro dato");
+//     console.log(entrada1);
+// }
